e9552465cd506e5b8301bb88ed1b7d3b67bbab52
fouadhal <fouad.hallomi@mail.huji.ac.il>
2020-12-27 04:41:45 +0200

    island sink 'animation'

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public float perc;
+    private float timeGameStarted;
+
+    public GameManager()
+    {         
+        timeGameStarted = -1;
+    }
+
+    public float getTimeStarted()
+    {
+        if (timeGameStarted == -1)
+            timeGameStarted = Time.time;
+
+        return timeGameStarted;
+    }
+
+    public float getIslandSinkPercentage()
+    {
+        return perc;
+    }
+}
---
+++ Assets/Scripts/Water2DScript.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water2DScript : MonoBehaviour
+{
+    public Vector2 MaxSpeed = new Vector2(0.01f, 0.01f);
+    public float testSinkPercentage;
+
+    private MeshRenderer renderer;
+    private Material material;
+    private Color waterColor;
+    private float shaderMagnitude;
+
+    void Awake()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        material = renderer.material;
+        waterColor = material.color;
+        shaderMagnitude = material.GetFloat("_Magnitude");
+    }
+
+    void LateUpdate()
+    {
+        //float sinkPerc = GameManager.Instance.getIslandSinkPercentage();
+        float sinkPerc = testSinkPercentage;
+        material.color = Color.white + sinkPerc * (waterColor - Color.white);
+        material.SetFloat("_Magnitude", sinkPerc * shaderMagnitude);
+        Vector2 scroll = Time.deltaTime * (sinkPerc * MaxSpeed);
+        material.mainTextureOffset += scroll;
+    }
+}
